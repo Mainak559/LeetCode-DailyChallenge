5230478e08e260a67cf9b7b25230e076359ac916
Mainak Halder <mainakhalder5598633@gmail.com>
2024-02-22 16:08:25 +0530

    22th feb

---
+++ 2024/2.Feb/15.Feb22'24.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/find-the-town-judge/
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        // Arrays to track trust given and received
+        vector<int> trusting(n + 1, 0);   
+        vector<int> trusted(n + 1, 0);     
+        for (auto i : trust) {                    // Count trust relationships
+            trusting[i[0]]++;                    // Increment trust given
+            trusted[i[1]]++;                    // Increment trust received
+        }
+        int ans = -1;                            // Initialize result
+                       
+        for (int i = 1; i <= n; i++)           // Find the person trusted by everyone else
+            if (trusting[i] == 0 && trusted[i] == n - 1){ 
+                ans = i;             // Update result
+            }
+        return ans;                // Return judge's index or -1 if not found
+    }
+};
