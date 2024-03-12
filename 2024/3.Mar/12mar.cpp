4b826873f477f1f7d501e42d25202075b313f764
Mainak Halder <mainakhalder5598633@gmail.com>
2024-03-12 11:25:45 +0530

    12 mar

---
+++ 2024/3.Mar/12mar.cpp
@@ -0,0 +1,40 @@
+//https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/description/?envType=daily-question&envId=2024-03-12
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        unordered_map<int, ListNode*> prefixSumToNode;
+        int prefixSum = 0;
+        for (ListNode* current = dummy; current != nullptr; current = current->next) {
+            prefixSum += current->val;
+            if (prefixSumToNode.count(prefixSum)) {
+                ListNode* prev = prefixSumToNode[prefixSum];
+                ListNode* toRemove = prev->next;
+                int p = prefixSum + (toRemove ? toRemove->val : 0);
+                while (p != prefixSum) {
+                    prefixSumToNode.erase(p);
+                    toRemove = toRemove->next;
+                    p += (toRemove ? toRemove->val : 0);
+                }
+                prev->next = current->next;
+            } else {
+                prefixSumToNode[prefixSum] = current;
+            }
+        }
+        return dummy->next;
+    }
+};
