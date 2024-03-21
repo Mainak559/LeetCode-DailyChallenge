b6eeb847b7df07ad887c050989023edafe999a48
Mainak Halder <mainakhalder5598633@gmail.com>
2024-03-21 23:50:44 +0530

    21 mar

---
+++ 2024/3.Mar/21mar.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/reverse-linked-list/description/?envType=daily-question&envId=2024-03-21
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+          ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+};
